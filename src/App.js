22fd8895ff262f3d60fbc900601d7f0595ef7d1c
jeahun10717 <jeahun10717@gmail.com>
2021-06-23 17:40:04 +0900

    2021 react pracice - omok first upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import Plate from "./components/Plate"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const max = 15;
+const plateStateArr = Array.from(Array(max), () => Array(max).fill(0))
+
+
+class App extends React.Component{
+  state = {
+    plateState: plateStateArr
+  }
+  
+  render(){
+    return (
+      <div>
+        <Plate/>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/Plate.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Plate.css"
+
+
+const arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+
+class Plate extends React.Component{
+    
+    render(){
+        return <div>
+            <table key="omokPlate" className="omokPlate">
+                <tbody>
+                {arr.map((i)=>(
+                    <tr key = {i}>
+                        {arr.map((j)=>(
+                            <td key={j} className={j}><button></button></td>
+                        ))}
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+        </div>
+    }
+}
+
+export default Plate;
