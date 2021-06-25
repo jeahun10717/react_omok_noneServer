37652c1d73a9ccb75348645b25a7221a7ae00158
jeahun10717 <jeahun10717@gmail.com>
2021-06-25 23:16:57 +0900

    omok upload in 2021 6 25

--- src/App.js
+++ src/App.js
@@ -6,14 +6,43 @@ const plateStateArr = Array.from(Array(max), () => Array(max).fill(0))
 
 
 class App extends React.Component{
-  state = {
-    plateState: plateStateArr
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      plateState: plateStateArr,
+      turn: 1, // turn 1 은 흑돌, turn -1 은 백돌
+      stoneState: 0,
+      stoneColor: " "
+    }
+    this.turnChange=this.turnChange.bind(this)
   }
-  
+
+  turnChange(i, j){
+    console.log(i, j);
+    const row = i;
+    const col = j;
+    // console.log(this.state.plateState);
+    // console.log(this.state.plateState[i][j]);
+    // if(this.state.plateState[row][col]===0){
+    //   console.log("taldjalfgjkdalfj");
+    // }
+    console.log(this.state.plateState.Array[0]);
+    // if(this.state.plateState[i][j]===0){
+    //   if(this.state.turn===1){
+    //     this.setState({
+    //       plateState.[i][j]: -1
+    //     })
+    //   }else if(this.state.turn===-1){ 
+        
+    //   }
+    // }
+  }
+
   render(){
     return (
       <div>
-        <Plate/>
+        <Plate change={this.turnChange} />
       </div>
     );
   }
--- src/components/Plate.js
+++ src/components/Plate.js
@@ -5,16 +5,20 @@ import "./Plate.css"
 const arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 
 class Plate extends React.Component{
-    
+
     render(){
         return <div>
             <table key="omokPlate" className="omokPlate">
                 <tbody>
                 {arr.map((i)=>(
-                    <tr key = {i}>
-                        {arr.map((j)=>(
-                            <td key={j} className={j}><button></button></td>
-                        ))}
+                    <tr key = {i} className={i}>
+                        {
+                            arr.map((j)=>(
+                                <td key={j} className={j}>
+                                    <button onClick={()=>(this.props.change(i, j))}></button>
+                                </td>
+                            ))
+                        }
                     </tr>
                 ))}
                 </tbody>
@@ -23,4 +27,4 @@ class Plate extends React.Component{
     }
 }
 
-export default Plate;
+export default Plate; 
