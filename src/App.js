0af8a0e68725504b834a156c42382a4dd76c9d78
jeahun10717 <jeahun10717@gmail.com>
2021-06-28 17:48:11 +0900

    front function complete upload

--- src/App.js
+++ src/App.js
@@ -10,39 +10,39 @@ class App extends React.Component{
   constructor(props) {
     super(props);
     this.state = {
-      plateState: plateStateArr,
-      turn: 1, // turn 1 은 흑돌, turn -1 은 백돌
-      stoneState: 0,
-      stoneColor: " "
+      plateState: plateStateArr,// 0 은 안놓아진 상태 , 1은 흑돌 -1 은 백돌
+      turn: 0 // 0 일 때 흑돌차례, 1일 때 백돌차례
     }
     this.turnChange=this.turnChange.bind(this)
   }
 
-  turnChange(i, j){
-    console.log(i, j);
-    const row = i;
-    const col = j;
+  turnChange(i, j){// i, j 는 받아오는 좌표값
+    let tmpArr = plateStateArr;
     // console.log(this.state.plateState);
-    // console.log(this.state.plateState[i][j]);
-    // if(this.state.plateState[row][col]===0){
-    //   console.log("taldjalfgjkdalfj");
-    // }
-    console.log(this.state.plateState.Array[0]);
-    // if(this.state.plateState[i][j]===0){
-    //   if(this.state.turn===1){
-    //     this.setState({
-    //       plateState.[i][j]: -1
-    //     })
-    //   }else if(this.state.turn===-1){ 
-        
-    //   }
-    // }
+    if(this.state.plateState[i][j]===0){
+      if(this.state.turn===0){
+        tmpArr[i][j]=1
+        this.setState({
+          plateState:tmpArr,
+          turn:1
+        })
+        // console.log(this.state.plateState[i][j]);
+      }else{
+        tmpArr[i][j]=-1
+        this.setState({
+          plateState:tmpArr,
+          turn:0
+        }) 
+      }
+    }else{
+      return;
+    }
   }
 
   render(){
     return (
       <div>
-        <Plate change={this.turnChange} />
+        <Plate change={this.turnChange} plateArr={this.state.plateState} />
       </div>
     );
   }
--- src/components/Plate.js
+++ src/components/Plate.js
@@ -1,11 +1,13 @@
 import React from "react";
 import "./Plate.css"
 
-
 const arr = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14]
 
-class Plate extends React.Component{
+function setStone(props) {
+    
+}
 
+class Plate extends React.Component{
     render(){
         return <div>
             <table key="omokPlate" className="omokPlate">
@@ -15,7 +17,15 @@ class Plate extends React.Component{
                         {
                             arr.map((j)=>(
                                 <td key={j} className={j}>
-                                    <button onClick={()=>(this.props.change(i, j))}></button>
+                                    <button onClick={()=>(this.props.change(i, j))}>
+                                        {
+                                            (function() {
+                                                if(this.props.plateArr[i][j]===0) return(<div></div>)
+                                                if(this.props.plateArr[i][j]===1) return(<div>⚫️</div>)
+                                                if(this.props.plateArr[i][j]===-1) return(<div>⚪️</div>)
+                                            }.bind(this))()
+                                        }
+                                    </button>
                                 </td>
                             ))
                         }
