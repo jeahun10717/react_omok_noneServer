c85457d1b887f126dd6d2635fea3c1dbdc3530a1
jeahun10717 <jeahun10717@gmail.com>
2021-07-01 22:26:27 +0900

    winRule upload

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const prophibitRule = require('./rules/prohibitRule')
 
 const max = 15;
 const plateStateArr = Array.from(Array(max), () => Array(max).fill(0))
-
+const emptyPlate = Array.from(Array(max), () => Array(max).fill(0))
 
 class App extends React.Component{
 
@@ -19,18 +19,39 @@ class App extends React.Component{
   }
 
   turnChange(i, j){// i, j 는 받아오는 좌표값
-    let tmpArr = plateStateArr;
+    let tmpArr = plateStateArr; // 얕은 복사 : 기본적으로 tmpArr 와 plateStateArr 는 값을 공유한다(주소가 같은 얕은복사이기 때문)
+    let winResult;
     // console.log(this.state.plateState);
     if(this.state.plateState[i][j]===0){
       if(this.state.turn===0){
         tmpArr[i][j]=1
+        winResult = winRule.winRule(this.state.turn, tmpArr, max)
+        console.log(winResult);
+        if(winResult==="prohibitRule"){ // 6목 이상일 때
+          console.log("6목 금지");
+          tmpArr[i][j]=0
+          this.setState({turn:0})
+        }else if(winResult==="blackWin"){
+          window.location.href = "http://localhost:3000/"
+        }else if(winResult==="continue"){
+        this.setState({
+          plateState:tmpArr,
+          turn:1
+        })}
         this.setState({
           plateState:tmpArr,
           turn:1
         })
-        // console.log(this.state.plateState[i][j]);
       }else{
         tmpArr[i][j]=-1
+        winResult = winRule.winRule(this.state.turn, tmpArr, max)
+        if(winResult==="whiteWin"){
+          window.location.href = "http://localhost:3000/"
+          // this.setState({
+          //   plateState:emptyPlate.map(v=>v.slice()),
+          //   turn:1
+          // })
+        }
         this.setState({
           plateState:tmpArr,
           turn:0
--- src/rules/winRule.js
+++ src/rules/winRule.js
@@ -1,11 +1,89 @@
-exports.winRule = (i, j, turn, stoneArr, max) => { 
+exports.winRule = (turn, stoneArr, max) => { 
     //i,j 는 2차원 배열의 index, turn은 흑인지 백인지, 
     //sonteArr 는 판의 상태(2차원 배열), max 는 판의 최대 크기 
-    const ruleCheckArr = stoneArr;
-    if(turn === 1){//백돌 차례일 때 승리조건 판단
+    const winCheckArr = stoneArr;
 
-    }else{//흑돌 차례일 때 승리조건 판단
-
+    if(turn === 0){//흑돌 차례일 때 승리조건 판단
+        for (let i = 0; i < max; i++) {
+            for (let j = 0; j < max; j++) {
+                if(// RIGHT
+                    winCheckArr[i][j]===1 && 
+                    winCheckArr[i][j+1]===1 && 
+                    winCheckArr[i][j+2]===1 && 
+                    winCheckArr[i][j+3]===1 && 
+                    winCheckArr[i][j+4]===1){
+                        if(winCheckArr[i][i+5]===1){
+                            return "prohibitRule"
+                        }
+                    alert("흑 승리!")
+                    return "blackWin"
+                }else if( // RIGHT DOWN
+                    winCheckArr[i][j]===1 && 
+                    winCheckArr[i+1][j+1]===1 && 
+                    winCheckArr[i+2][j+2]===1 && 
+                    winCheckArr[i+3][j+3]===1 && 
+                    winCheckArr[i+4][j+4]===1){
+                    alert("흑 승리!")            
+                    return "blackWin"
+                }else if( // DOWN
+                    winCheckArr[i][j]===1 && 
+                    winCheckArr[i+1][j]===1 && 
+                    winCheckArr[i+2][j]===1 && 
+                    winCheckArr[i+3][j]===1 && 
+                    winCheckArr[i+4][j]===1){
+                    alert("흑 승리!")
+                    return "blackWin"
+                }else if( // LEFT DOWN
+                    winCheckArr[i][j]===1 && 
+                    winCheckArr[i+1][j-1]===1 && 
+                    winCheckArr[i+2][j-2]===1 && 
+                    winCheckArr[i+3][j-3]===1 && 
+                    winCheckArr[i+4][j-4]===1){
+                    alert("흑 승리!")
+                    return "blackWin"
+                }else{ // winrule 에 걸리지 않을 경우
+                    return "continue"
+                }          
+            }
+        }
+    }else if(turn === 1){//백돌 차례일 때 승리조건 판단
+        for (let i = 0; i < max; i++) {
+            for (let j = 0; j < max; j++) {
+                if(// LEFT
+                    winCheckArr[i][j]===-1 && 
+                    winCheckArr[i][j+1]===-1 && 
+                    winCheckArr[i][j+2]===-1 && 
+                    winCheckArr[i][j+3]===-1 && 
+                    winCheckArr[i][j+4]===-1){
+                    alert("백 승리!")
+                    return "whiteWin"
+                }else if( // LEFT DOWN
+                    winCheckArr[i][j]===-1 && 
+                    winCheckArr[i+1][j+1]===-1 && 
+                    winCheckArr[i+2][j+2]===-1 && 
+                    winCheckArr[i+3][j+3]===-1 && 
+                    winCheckArr[i+4][j+4]===-1){
+                    alert("백 승리!")
+                    return "whiteWin"            
+                }else if( // DOWN
+                    winCheckArr[i][j]===-1 && 
+                    winCheckArr[i+1][j]===-1 && 
+                    winCheckArr[i+2][j]===-1 && 
+                    winCheckArr[i+3][j]===-1 && 
+                    winCheckArr[i+4][j]===-1){
+                    alert("백 승리!")
+                    return "whiteWin"
+                }else if( // RIGHT DOWN
+                    winCheckArr[i][j]===-1 && 
+                    winCheckArr[i+1][j-1]===-1 && 
+                    winCheckArr[i+2][j-2]===-1 && 
+                    winCheckArr[i+3][j-3]===-1 && 
+                    winCheckArr[i+4][j-4]===-1){
+                    alert("백 승리!")
+                    return "whiteWin"
+                }          
+            }
+        }
     }
 }
 
